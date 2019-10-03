07821b3dbd3b1540f8c522d35de10f693b1bb3a5
anthony.albertorio <anthony.albertorio@gmail.com>
2019-10-03 11:42:13 -0400

    added code for double array example.

---
+++ code/doubleArr.js
@@ -0,0 +1,10 @@
+function double(arr) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(2 * arr[i]);
+  }
+  return newArr;
+}
+
+const arg = process.argv[2];
+console.log(double(arg));
